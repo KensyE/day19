ee4c39348ea591241eac55aba5a40155f896e52c
KensyE <kensyesky24@gmail.com>
2025-07-29 22:40:12 +0530

    Your message

--- index.js
+++ index.js
@@ -1,57 +1,74 @@
-// const http = require('http');
+// console.log('Hello, World!');
+const express=require ('express')
+const app=express()
+const port=3000
 
-// const server = http.createServer((req, res) => {
-//   // Parse Request
-//   const method = req.method;         // GET, POST, etc.
-//   const url = req.url;               // /about, /home, etc.
+app.get('/',(req, res)=> {
+  res.send('Hello expresso')
+})
 
-//   // Send Response
-//   res.setHeader('Content-Type', 'text/plain'); // Set response type
-//   res.write(`You made a ${method} request to ${url}`); // Response body
-//   res.end(); // Finalize response
-// });
+// app.post('/home', (req, res) => {
+//   res.send('Got a POST request')
+// })
 
-// server.listen(3000, () => {
-//   console.log('Server is running on http://localhost:3000');
-// });
-
-// let body = '';
-// req.on('data', chunk => {
-//   body += chunk.toString(); // convert Buffer to string
-// });
-
-// req.on('end', () => {
-//   console.log('Received data:', body);
-//   res.end('Data received');
-// });
-// res.setHeader('Content-Type', 'application/json');
-// res.end(JSON.stringify({ message: 'Hello from Node.js!' }));
-//   if (req.url === '/home') {
-//     res.end('Welcome to the homepage!');
-//   } else if (req.url === '/about') {
-//     res.end('About us page');
-//   } else {
-//     res.statusCode = 404;
-//     res.end('Page not found');
-//   }
-// ;
-// let http = require('http');
-
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hello World!');
-// }).listen(8080);
-
-const fs = require('fs');
-
-// Read file asynchronously
-fs.readFile('myfile.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file: ' + err);
-    return;
-  }
-  console.log('File content: ' + data);
+app.get('/users/:username', (req, res) => {
+  const username = req.params.username;
+  res.send(`Hello, ${username}!`);
 });
 
-console.log('Reading file... (this runs first!)');
+app.all('/about', (req, res, next) => {
+  console.log('Accessing the about section ...')
+  next()
+})
+
+app.get('/example/a/:maybe', (req, res) => {
+  const maybe = req.params.maybe;
+  res.send(`Hello from A! Maybe: ${maybe}`);
+})
+
+app.get('/example/b/:maybe', (req, res) => {
+  const maybe = req.params.maybe;
+  res.send(`Hello from B! Maybe: ${maybe}`);
+  app.get('/user/:id', (req, res, next) => {
+  console.log('ID:', req.params.id)
+  next()
+}, (req, res, next) => {
+  res.send('User Info')
+})
+
+// handler for the /user/:id path, which prints the user ID
+app.get('/user/:id', (req, res, next) => {
+  res.send(req.params.id)
+})
+
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
